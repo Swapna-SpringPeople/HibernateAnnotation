38b183119f2cbe3bb4fba524a2dc8e33904c5b56
Swapna-SpringPeople <swapnaspringjavascript@gmail.com>
2017-03-23 09:57:29 +0530

    Hibernate annotation

---
+++ PersonManager.java
@@ -0,0 +1,37 @@
+package com.sakha.hibernate.manager;
+
+import com.sakha.hibernate.model.Address;
+import com.sakha.hibernate.model.Person;
+import com.sakha.hibernate.util.HibernateUtil;
+ 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+ 
+public class PersonManager {
+ 
+    public static void main(String[] args) {
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session session = sf.openSession();
+        session.beginTransaction();
+ 
+        Person steve = new Person("Steve", "Jobs");
+        Person donald = new Person("Donald", "Trump");
+ 
+        Address valley = new Address("Steve P Jobs", "San Francisco", "11111");
+        Address newyork = new Address("Trump Tower", "New York", "22222");
+        Address chicago = new Address("Trump Tower", "Chicago", "33333");
+ 
+        steve.getAddresses().add(valley);
+        donald.getAddresses().add(newyork);
+        donald.getAddresses().add(chicago);
+ 
+        System.out.println("Creating Person: " + steve.getFirstName());
+        session.persist(steve);
+        System.out.println("Creating Person: " + donald.getFirstName());
+        session.persist(donald);
+ 
+        session.getTransaction().commit();
+        session.close();
+    }
+ 
+}
